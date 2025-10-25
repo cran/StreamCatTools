baf690c9885def8c67d6d2d69bdf9ea9d9c65654
Marc Weber <weber.marc@epa.gov>
2025-10-25 07:30:22 +0000

    version 0.9.1

---
+++ inst/doc/StartHere.R
@@ -0,0 +1,21 @@
+## ----include = FALSE----------------------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>", 
+  warning = FALSE, 
+  message = FALSE
+)
+
+## ----message=FALSE, eval=FALSE------------------------------------------------
+# install.packages("StreamCatTools")
+
+## ----message=FALSE, eval=FALSE------------------------------------------------
+# library(devtools)
+# install_github('USEPA/StreamCatTools')
+
+## ----message=FALSE------------------------------------------------------------
+library(StreamCatTools)
+
+## -----------------------------------------------------------------------------
+citation("StreamCatTools")
+
